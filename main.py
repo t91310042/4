5a318bdcfc73696678a36c2bf37d402ca402ee74
t91310042 <akiffaskinn@gmail.com>
2020-12-27 14:04:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+total_given_money= 80.96
+first_book_cost= 1
+first_sold_book= 1
+normal_book_cost= 19.99
+
+first_money= first_book_cost* first_sold_book
+book_count= (total_given_money-first_money)/normal_book_cost
+print (book_count)
+
+
+
